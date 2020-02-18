c4d11cbd95b0b5c9aa112cc07f90415c250e16ba
julienlamotte <61149163+julienlamotte@users.noreply.github.com>
2020-02-18 22:21:47 +0100

    Update labo00.c

--- labo00.c
+++ labo00.c
@@ -1,1 +1,27 @@
-// labo 00
+#include <stdio.h> 
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#define taille 50
+
+int16_t main(void)
+{
+	char chaine[taille];
+	int16_t arguments = 0;
+
+	fgets(chaine, taille, stdin);
+
+	for (int16_t i = 0; i < taille; i++)
+	{
+		if (chaine[i] == ' ' || chaine[i] == '\0');
+		{
+			chaine[i] = '\n';
+			arguments++;
+		}
+
+	}
+	printf("%d", arguments);
+	printf("%s", chaine);
+
+	return 0;
+}
